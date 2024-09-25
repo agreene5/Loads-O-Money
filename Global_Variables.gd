b36bffbf004034f52e2ea13dca86e52fe542312a
agreene5 <greene.adam5135@gmail.com>
2024-09-24 23:35:02 -0400

    Bouncy Coins Implemented
    Also, reformatted a bit of the other stuff, but any code you made before this should still work with this

--- Global_Variables.gd
+++ Global_Variables.gd
@@ -1,5 +1,8 @@
 # Add variables that are needed by multiple scripts here (ex. Money)
 extends Node
 
-var deceleration = 5.0 # Defines deceleration of bullets over time
+var deceleration = 5.0 # Defines deceleration of bullets over time (if you're using a rigidbody2D you can use the damp value under the Linear tab instead
 
+var money = 10.0 # Defines the players money value (default value starts at $10)
+
+var Coin_Variant = 3  # Determines the coin variant the user has | 0: Penny, 1: Nickel, 2: Dime, 3: Quarter
--- Player_Shot_Script.gd
+++ Player_Shot_Script.gd
@@ -1,59 +1,125 @@
 # This script defines the 3 shot types for the character, which are cycled through by pressing
-# the "E" key.
+# the "E" key. It also subtracts from the money variable an amount determined by the shot type
+# every time you shoot.
 
 extends Node2D
 
-var CoinShot = preload("res://player_coin_shot.tscn")
-# var DollarShot = preload("res://player_dollar_shot.tscn")
-# var CheckShot = preload("res://player_check_shot.tscn")
+var CoinShotPenny = preload("res://player_penny_coin_shot.tscn")
+var CoinShotNickel = preload("res://player_nickel_coin_shot.tscn")
+var CoinShotDime = preload("res://player_dime_coin_shot.tscn")
+var CoinShotQuarter = preload("res://player_quarter_coin_shot.tscn")
+
+# var DollarShotWashington = preload()
+# var DollarShotLincoln = preload()
+# var DollarShotJackson = preload()
+# var DollarShotGrant = preload()
+
+# var CheckShot100 = preload()
+# var CheckShot200 = preload()
+# var CheckShot500 = preload()
+# var CheckShot1000 = preload()
+
+var Coin_Variant = Global_Variables.Coin_Variant
+var coin_costs = [0.01, 0.05, 0.10, 0.01] # Determines the cost per shot of each coin
+
+#var Dollar_Variant
+#var dollar_costs
+
+#var Check_Variant
+#var check_costs
 
 enum ShotType {COIN, DOLLAR, CHECK}
-var current_shot_type = ShotType.COIN # Default shot type
+var current_shot_type = ShotType.COIN  # Start with coin type as default
+
+var is_firing = false
+var fire_timer = 0.0
+
+var money = Global_Variables.money
 
 func _input(event):
-		if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-				match current_shot_type:
-						ShotType.COIN:
-								_handle_coin_shot(event)
-						ShotType.DOLLAR:
-								_handle_dollar_shot(event)
-						ShotType.CHECK:
-								_handle_check_shot(event)
-		
-		# Press "E" in order to cycle through the shot types
-		if event is InputEventKey and event.pressed and event.keycode == KEY_E:
-				cycle_shot_type()
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		is_firing = event.pressed
+		if event.pressed:
+			attempt_fire()
+	
+	# Press E to cycle through the shot types (Coin, Dollar, Check)
+	if event is InputEventKey and event.pressed and event.keycode == KEY_E:
+		cycle_shot_type()
 
-func _handle_coin_shot(event):
-		var coin_instance = CoinShot.instantiate()
+func _process(delta):
+	fire_timer += delta
+	if is_firing:
+		attempt_fire()
+
+func attempt_fire():
+	if money <= 0:
+		return  # You can't shoot if you don't have money (but you should go bankrupt and lose before this occurs in the final product)
+
+	var fire_rate = get_fire_rate()
+	if fire_timer >= 1.0 / fire_rate:
+		fire_shot()
+		fire_timer = 0.0
+
+func fire_shot():
+	match current_shot_type:
+		ShotType.COIN:
+			var coin_type
+			var cost = coin_costs[Coin_Variant]
+			match Coin_Variant:
+				0:
+					coin_type = CoinShotPenny
+				1:
+					coin_type = CoinShotNickel
+				2:
+					coin_type = CoinShotDime
+				3:
+					coin_type = CoinShotQuarter
+			_handle_coin_shot(coin_type, cost)
+		ShotType.DOLLAR:
+			# 
+			#
+			#
+			_handle_dollar_shot()
+		ShotType.CHECK:
+			#
+			#
+			#
+			_handle_check_shot()
+
+func get_fire_rate():
+	match current_shot_type:
+		ShotType.COIN:
+			return 8.0 
+		ShotType.DOLLAR:
+			return 3.0
+		ShotType.CHECK:
+			return 0.33
+
+func _handle_coin_shot(coin_type, cost):
+	if money >= cost:
+		money -= cost
+		var coin_instance = coin_type.instantiate()
 		var parent = get_parent()
 		
-		var player_sprite = parent.get_node("Player_Sprite")
+		var player_sprite = parent.get_node("Player_Sprite") # Shooting in relation to the player sprite location
 		coin_instance.global_position = player_sprite.global_position
 		
 		var click_position = get_global_mouse_position()
 		var direction = (click_position - player_sprite.global_position).normalized()
 		
 		coin_instance.rotation = direction.angle()
-		
 		coin_instance.set_initial_direction(direction)
 
 		get_tree().current_scene.add_child(coin_instance)
 
-func _handle_dollar_shot(event):
-		# Code for dollar shot
-		pass
+func _handle_dollar_shot():
+	# Dollar shot code
+	pass
 
-func _handle_check_shot(event):
-		# Code for check shot
-		pass
+func _handle_check_shot():
+	# Check shot code
+	pass
 
-# Function to cycle through shot types
 func cycle_shot_type():
-		match current_shot_type:
-				ShotType.COIN:
-						current_shot_type = ShotType.DOLLAR
-				ShotType.DOLLAR:
-						current_shot_type = ShotType.CHECK
-				ShotType.CHECK:
-						current_shot_type = ShotType.COIN
+	# Cycle through the shot types (Coin, Dollar, Check)
+	current_shot_type = (current_shot_type + 1) % len(ShotType)
