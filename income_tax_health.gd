ae4dafae39a0dd4aec0c42e3e166594d80905c74
JoeB1d0me <96071431+JoeB1d0me@users.noreply.github.com>
2024-10-04 20:09:51 -0400

    update to global variable function

--- income_tax_health.gd
+++ income_tax_health.gd
@@ -3,20 +3,17 @@ var income_tax_health = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	area_entered.connect(_on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	area_entered.connect(_on_area_entered)
+	pass
 
 func despawn():
 	if income_tax_health <= 0:
 		queue_free()
-		
-		
 
 func _on_area_entered(area) -> void:
 	Global_Variables.hit(area,income_tax_health)
-	print("Test")
 	
