0be8bd06db741760b6c869015095ed34da47e147
matzoman7 <alexauld118@gmail.com>
2024-10-15 23:25:41 -0400

    Added a pause menu
    It's not animated yet, but it pauses the game, buttons work, and it has the new graphic.

---
+++ UI + Menus + Scenes/Pause_Menu/pause_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var pause_menu = $"."
+var paused = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("esc"):
+		pauseMenu()
+
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale =1
+	else:
+		pause_menu.show()
+		Engine.time_scale =0
+	paused = !paused
+	
+
+
+
+func _on_resume_pressed() -> void:
+	pauseMenu()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI + Menus + Scenes/Main_Menu/main_menu.tscn")
