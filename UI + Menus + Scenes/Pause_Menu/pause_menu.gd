da4c9beab27f93c565fde3e6510d5b6388c17718
matzoman7 <alexauld118@gmail.com>
2024-10-27 16:07:29 -0400

    Removed error in code
    Took out the tween's that were not working.

--- UI + Menus + Scenes/Pause_Menu/pause_menu.gd
+++ UI + Menus + Scenes/Pause_Menu/pause_menu.gd
@@ -10,14 +10,14 @@ func _process(delta):
 
 func pauseMenu():
 	if paused:
-		var tween = create_tween()
+		
 		pause_menu.hide()
-		tween.tween_property(todd, "position", Vector2(20,0), 1)
+		
 		Engine.time_scale =1
 	else:
-		var tween = create_tween()
+		
 		pause_menu.show()
-		tween.tween_property(todd, "position", Vector2(-200,0), 5)
+		
 		
 		Engine.time_scale =0
 	paused = !paused
