aba2fa647210b979d8c38807e438f832093e4839
agreene5 <greene.adam5135@gmail.com>
2024-12-05 23:29:00 -0500

    Just kidding, this should be the final one

--- Misc/Global_Variables.gd
+++ Misc/Global_Variables.gd
@@ -33,7 +33,7 @@ var player_job = 0 # Defines the players current job | 0: Unemployed, 1: Fast Fo
 var player_job_values = { # Defines player job stat values
 	# [0-Exp needed to move up a job, 1-money you earn/second, 2-dash cooldown, 3-dash speed, 4-dash time, 5-speed, 6-sprite, 7-red flash threshold
 	0: [3.0, 0.00, 2.0, 400.0, 0.5, 200.0, "res://Finished_Assets/Player_Body_Assets/Unemployed_Todd.png", 1.50],
-	1: [50.0, 0.30, 1.5, 550.0, 0.4, 230.0, "res://Finished_Assets/Player_Body_Assets/Fast_Food_Worker_Todd.png", 1.00],
+	1: [50.0, 0.4, 1.5, 550.0, 0.4, 230.0, "res://Finished_Assets/Player_Body_Assets/Fast_Food_Worker_Todd.png", 1.00],
 	2: [500.0, 6.00, 1.3, 700.0, 0.3, 267.0, "res://Finished_Assets/Player_Body_Assets/Restaurant_Manager_Todd.png", 2.00],
 	3: [2000.0, 50.00, 1.0, 1000.0, 0.2, 300.0, "res://Finished_Assets/Player_Body_Assets/Regional_Operations_Manager_Todd.png", 20.00],
 	4: [5000.0, 200.00, 0.8, 1500.0, 0.15, 330.0, "res://Finished_Assets/Player_Body_Assets/CEO_Todd.png", 200.00],
